276ef699d66c041c8c959a94664bc928286924ee
L1oid <71085120+L1oid@users.noreply.github.com>
2022-05-29 02:08:09 +0700

    build 0.6

--- src/database/Const.java
+++ src/database/Const.java
@@ -7,4 +7,7 @@ public class Const {
     public static final int GET_CARDS = 103;
     public static final int GET_UNITS = 104;
     public static final int PLAYER_ACTION = 105;
+    public static final int INVALID_LOGIN = 200;
+    public static final int WRONG_PASSWORD = 201;
+    public static final int SUCCESS_LOGIN = 202;
 }
